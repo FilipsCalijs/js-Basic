9035714eaff993c7c1a9d3995d267e0d065835d0
FilipsCalijs <filipscalijs@gmail.com>
2023-11-21 09:02:35 +0200

    slider is done (2nd var)

--- js/script.js
+++ js/script.js
@@ -310,6 +310,7 @@ window.addEventListener('DOMContentLoaded', function() {
     let slideIndex = 1;
     let offset = 0;
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider');
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -327,15 +328,61 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     
 
-        slidesField.style.width = 100 * slides.length + '%';
-        slidesField.style.display = 'flex';
-        slidesField.style.transition = '0.5s all';
+        
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
 
+    slidesWrapper.style.overflow = 'hidden';
 
-        slidesWrapper.style.overflow = 'hidden';
-        slides.forEach(slides => {
-            slides.style.width = width;
-        });
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; 
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
         next.addEventListener("click", () => {
             if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
                 offset = 0;
@@ -356,6 +403,9 @@ window.addEventListener('DOMContentLoaded', function() {
             } else {
                 current.textContent = slideIndex;
             }
+
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
         });
     
         prev.addEventListener("click", () => {
@@ -378,6 +428,9 @@ window.addEventListener('DOMContentLoaded', function() {
             } else {
                 current.textContent = slideIndex;
             }
+
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
         });
     });
 //     showSlides(slideIndex);
