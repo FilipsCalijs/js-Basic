1018fc47ff8e2115eb3f160cd1ddb6fb8ac722c7
FilipsCalijs <filipscalijs@gmail.com>
2023-11-22 21:53:10 +0200

    97 lesson - calc 1 part

--- js/script.js
+++ js/script.js
@@ -382,57 +382,152 @@ window.addEventListener('DOMContentLoaded', function() {
         indicators.append(dot);
         dots.push(dot);
     }
+    
 
-        next.addEventListener("click", () => {
-            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
-                offset = 0;
-            } else {
-                offset += +width.slice(0, width.length - 2);
-            }
-    
-            slidesField.style.transform = `translateX(-${offset}px)`;
-    
-            if (slideIndex == slides.length) {
-                slideIndex = 1;
-            } else {
-                slideIndex++;
-            }
-    
-            if (slides.length < 10) {
-                current.textContent = `0${slideIndex}`;
-            } else {
-                current.textContent = slideIndex;
-            }
+    next.addEventListener('click', () => {
+        if (offset == (deleteNotDigits(width) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += deleteNotDigits(width); 
+        }
 
-            dots.forEach(dot => dot.style.opacity = ".5");
-            dots[slideIndex-1].style.opacity = 1;
-        });
-    
-        prev.addEventListener("click", () => {
-            if (offset == 0) {
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
-            } else {
-                offset -= +width.slice(0, width.length - 2);
-            }
-    
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = deleteNotDigits(width) * (slides.length - 1);
+        } else {
+            offset -= deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+
             slidesField.style.transform = `translateX(-${offset}px)`;
-    
-            if (slideIndex == 1) {
-                slideIndex = slides.length;
-            } else {
-                slideIndex--;
-            }
-    
+
             if (slides.length < 10) {
-                current.textContent = `0${slideIndex}`;
+                current.textContent =  `0${slideIndex}`;
             } else {
-                current.textContent = slideIndex;
+                current.textContent =  slideIndex;
             }
 
             dots.forEach(dot => dot.style.opacity = ".5");
             dots[slideIndex-1].style.opacity = 1;
         });
     });
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
+    
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height, weight, age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____'; // Можете придумать что угодно
+            return;
+        }
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+    
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case "height":
+                    height = +input.value;
+                    break;
+                case "weight":
+                    weight = +input.value;
+                    break;
+                case "age":
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+});
 //     showSlides(slideIndex);
 //     slides.forEach(item => console.log(item))
 //     console.log(slides, prev, next)
@@ -447,6 +542,7 @@ window.addEventListener('DOMContentLoaded', function() {
 //     slides.forEach(item => item.style.display = 'none');
 
 //     slides[slideIndex-1].style.display = 'block';
+
 //     }
 
 //     function plussSlides(n){
